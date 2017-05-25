d5b7ed28ada1bba136072c0937e2f91be739d248
Ciara Cagney <ciaracagney@Junes-MacBook-Pro.local>
2017-05-24 23:34:13 -0400

    some map stuff added

--- MarkovChain.java
+++ MarkovChain.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class MarkovChain {
 	//public MarkovChain(){ }
 
+	HashMap<Word, <List<Word>> map = new HashMap<Word, <List<Word>>;
 	static ArrayList<String> words = new ArrayList<String>(); 
 	private String string = "Hi there my name is aidan oakley and i really like to party and stuff.";
 	static int wordcount = 0;
@@ -34,39 +35,53 @@ public class MarkovChain {
 	 * @ciara
 	 *
 	 */
-	public int[] makeProbability(ArrayList<String> words) {
-			int[] probabilities; 
-			ArrayList<Integer> index = new ArrayList<Integer>();
-			ArrayList<String> str = new ArrayList<String>();
+	public void createMap(){
+
+		for(int i = 0; i < string.size(); i++){
+			map.put(string.get(i), new List("", "")); //not entirely sure what goes here 
+			map.get(i).add("");
+		}
+	}
+	public int[] makeProbability(ArrayList<String> words){
+		ArrayList<Integer> index = new ArrayList<Integer>();
+		ArrayList<String> str = new ArrayList<String>();
+
+		for(String s: words){
+
+			String temp = s; 
 			
-			for(String s: words) {
-				String temp = s; 
+			for(int i = 0; i < words.size(); i++){    //go through the list. yeah find all instances of the word. 
 				
-				for(int i = 0; i < words.size(); i++){    //go through the list. yeah find all instances of the word. 
-					if(s.equals(words.get(i))){
-						index.add(i);
-					}
+				if(s.equals(words.get(i))){
+					index.add(i);
 				}
-
-				for(int ind : index){ 			//look at the two words after that word in each case. 
-					if(ind < words.size()-1){
-						str.add(words.get(ind+1));
-						str.add(words.get(ind+2));
-					} else if(ind < words.size()){
-						str.add(words.get(ind+1));
-						str.add(""); // what to add here?
-					} else {
-						str.add(""); // what to add here?
-					}
-				}
-
-				for(int i = 0; i < str.size()-1; i++){          //If any of those two words show up more than once, make that probability greater
-					for(int j = i+1; j < str.size(); j++){
-						if((str.get(i)).equals(str.get(j))){
-							//make probability greater
-						}
-					}
-				}
+			
 			}
+			
+			for(int ind : index){ 			//look at the two words after that word in each case. 
+				
+				if(ind < words.size()-1){
+					str.add(words.get(ind+1));
+					str.add(words.get(ind+2));
+				} else if(ind < words.size()){
+				
+					str.add(words.get(ind+1));
+				
+				}
+			
+			}
+			
+			for(int i = 0; i < str.size()-1; i++){          //If any of those two words show up more than once, make that probability greater
+			
+				for(int j = i+1; j < str.size(); j++){
+			
+					if((str.get(i)).equals(str.get(j))){
+						//make probability greater
+					}
+			
+				}
+			
+			}	
+		}
 	}
 }
