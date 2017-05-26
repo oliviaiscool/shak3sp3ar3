155d1b001c860c56d5b25a3bb12eef39e630b000
Ciara Cagney <ciaracagney@Junes-MacBook-Pro.local>
2017-05-26 12:29:37 -0400

    added file reading but need to also add a text file of sonnets.txt

--- src/MarkovChain.java
+++ src/MarkovChain.java
@@ -1,36 +1,81 @@
-package markovChain;
+package edu.exeter.cs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MarkovChain {
 	public static HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-	static String strr = "Hi there my name is aidan and my name";
+	static String strr = "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.";
 	static String str = strr.replaceAll("’", "");
+	static ArrayList<String> lines = new ArrayList<String>();
 	static int wordcount = 0;
 	public int[] probability = new int[wordcount];
 	
 	public static void main(String[] args) {
-		ArrayList<String> text = createList(str);
+		ArrayList<String> linTemp = readFile();
+		for(String s : linTemp){
+			lines.add(s.replaceAll("'", ""));
+		}
+		lines.add(str);
+		ArrayList<String> text = createList(lines);
 
 		HashMap<String, ArrayList<String>> textMap = createMap(text);
 		System.out.println(textMap);
 		
+		String thing = "Hi there my name is Aidan and my name is fuck";
+		
+		System.out.println(thing + " " + predict(thing));
+	}
+	
+	public static ArrayList<String> readFile(){
+		String line = null; 
+		ArrayList<String> list = new ArrayList<String>();
+		
+		try {
+            FileReader fileReader = 
+                new FileReader("sonnets.txt");
+
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+			while((line = bufferedReader.readLine()) != null) {
+                list.add(line);
+            }   
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Uh oh! Something went wrong :( file 'sonnets.txt' won't open.");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Uh oh! Something went wrong :( file 'sonnets.txt' won't open.");
+        }
+		
+		return list;
 	}
 
-	public static ArrayList<String> createList(String str) {
+	public static ArrayList<String> createList(ArrayList<String> str) {
 		ArrayList<String> words = new ArrayList<String>(); 
 		String delimiters = "[\\W]+";
-		String[] list = str.split(delimiters);
-		
-		for (String s: list) {
-
-			String wrd = s;
+		for(String s : str){
+			String[] list = s.split(delimiters);
 			
-			words.add(wrd);
-			wordcount++;
-		}
+			for (String st: list) {
 
+				String wrd = st;
+				
+				words.add(wrd);
+				wordcount++;
+			}
+		}
 		System.out.println(words.size());
 		return words; 
 	}
@@ -81,4 +126,3 @@ public class MarkovChain {
 		}
 	}
 }
- 
